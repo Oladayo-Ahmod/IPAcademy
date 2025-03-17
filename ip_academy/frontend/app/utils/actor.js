4679af702a5eade17b7cf1f5296a31186658ef00
Oladayo Ahmod <57647734+Oladayo-Ahmod@users.noreply.github.com>
2025-03-17 17:00:10 +0000

    feat : courses implementation

--- ip_academy/frontend/app/utils/actor.js
+++ ip_academy/frontend/app/utils/actor.js
@@ -1,15 +1,15 @@
 import { Actor, HttpAgent } from "@dfinity/agent";
 import { Principal } from "@dfinity/principal";
-import { idlFactory } from "../../../test/dfx_generated/hello_world/hello_world.did"
+import {idlFactory } from "./dfx_generated/hello_world/hello_world.did.js"
 const canisterId = process.env.NEXT_PUBLIC_CANISTER_ID;
 const host = process.env.NEXT_PUBLIC_IC_HOST;
 
-const agent = HttpAgent.create(host)
+const agent = new HttpAgent({host})
 
 // Fetch the root key for the local replica
-if (process.env.NODE_ENV !== "production") {
+// if (process.env.NODE_ENV !== "production") {
   agent.fetchRootKey();
-}
+// }
 
 const actor = Actor.createActor(idlFactory, {
   agent,
--- ip_academy/src/index.ts
+++ ip_academy/src/index.ts
@@ -1,4 +1,20 @@
-import { IDL, query, update } from 'azle';
+import {
+    IDL,query,update,StableBTreeMap,Principal
+  } from "azle";
+
+  import { v4 as uuidv4 } from "uuid";
+  
+  const Course =IDL.Record({
+    id: IDL.Nat,
+    title: IDL.Text,
+    description: IDL.Text,
+    instructor: IDL.Opt(IDL.Principal),
+    duration: IDL.Nat64,
+    skillLevel: IDL.Text,
+    prerequisites: Vec(text),
+    price: nat64,
+    students: Vec(Principal),
+  });
 
 export default class {
     message: string = 'Hello world!';
