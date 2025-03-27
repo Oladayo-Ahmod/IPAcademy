533cf4f6711d3a9ae7229c606352154f1c62a4f7
Oladayo Ahmod <57647734+Oladayo-Ahmod@users.noreply.github.com>
2025-03-27 22:04:48 +0000

    fix type error

--- ip_academy/frontend/app/courses/page.tsx
+++ ip_academy/frontend/app/courses/page.tsx
@@ -74,7 +74,7 @@ export default function CoursesPage() {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {courses.map((course) => {
             const isEnrolled = identity
-              ? course.students.some(student =>
+              ? course.students.some((student :any) =>
                 student.toString() === identity.getPrincipal().toString()
               )
               : false;
--- ip_academy/frontend/app/page.tsx
+++ ip_academy/frontend/app/page.tsx
@@ -196,7 +196,7 @@ export default function Home() {
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {courses.map((course) => {
               const isEnrolled = identity
-                ? course.students.some(student =>
+                ? course.students.some((student : any) =>
                   student.toString() === identity.getPrincipal().toString()
                 )
                 : false;
